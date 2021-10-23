b268fa03ec37fb8a62b11af74a8e5550fbabb1fa
Jeremy Rempel <jeremyrempel@gmail.com>
2021-10-22 23:10:10 -0400

    update datasource url

--- src/main/java/com/github/jeremyrempel/buysellhold/BuysellholdApplication.java
+++ src/main/java/com/github/jeremyrempel/buysellhold/BuysellholdApplication.java
@@ -1,13 +1,22 @@
 package com.github.jeremyrempel.buysellhold;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class BuysellholdApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BuysellholdApplication.class, args);
-	}
+    @Value("${userBucket.path}")
+    private String dbUrl;
 
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuysellholdApplication.class, args);
+    }
 }
